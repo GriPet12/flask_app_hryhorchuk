a9abaaf3d883076f2ef01d41fa06af92bacaef8e
Григорчук Петро Романович <pgrigorcuk0@gmail.com>
2024-10-23 11:06:04 +0300

    add static files

--- app.py
+++ app.py
@@ -1,50 +1,15 @@
-from flask import Flask, request, redirect, url_for, render_template, abort
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config.from_pyfile("config.py")
 
-
 @app.route('/')
-def hello_world():
-    return render_template("base.html")
-
-@app.route('/homepage')
 def home():
-    """View for the Home page of your website."""
-    agent = request.user_agent
+    return render_template('base.html', title="Home")
 
-    return render_template("home.html", agent=agent)
-
-@app.route("/hi/<string:name>")   #/hi/ivan?age=45&q=fdfdf
-def greetings(name):
-    name = name.upper()
-    age = request.args.get("age", 0, int)
-
-    return render_template("hi.html", name=name, age=age)
-
-@app.route("/admin")
-def admin():
-    to_url = "/hi/administrator?age=45"
-    to_url = url_for("greetings", name="administrator", age=45, _external=True)      # "/hi/administrator?age=45"
-    print(to_url)
-    return redirect(to_url)
-
-posts = [
-    {"id": 1, 'title': 'My First Post', 'content': 'This is the content of my first post.', 'author': 'John Doe'},
-    {"id": 2, 'title': 'Another Day', 'content': 'Today I learned about Flask macros.', 'author': 'Jane Smith'},
-    {"id": 3, 'title': 'Flask and Jinja2', 'content': 'Jinja2 is powerful for templating.', 'author': 'Mike Lee'}
-]
-
-@app.route('/posts')
-def get_posts():
-    return render_template("posts.html", posts=posts)
-
-@app.route('/post/<int:id>')
-def get_post(id):
-    if id > 3:
-        abort(404)
-    post = posts[id - 1]
-    return render_template("det_posts.html", post=post)
+@app.route('/resume')
+def resume():
+    return render_template('resume.html', title="My Resume")
 
 if __name__ == '__main__':
     app.run()
